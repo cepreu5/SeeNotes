206589a5dc5abbcbfc5704f52def331b6e82b11e
cepreu5 <cepreu5@live.com>
2025-12-02 03:39:07 +0200

    Mr. StickyMan

--- uni/msm.js
+++ uni/msm.js
@@ -3,12 +3,15 @@ const guideTexts = {
   en: [
     "<h2>Welcome!</h2><h3>I'm <b>Mr. StickyMan</b> - your guide to the world of sticky notes.</h3>",
     "Here you can add new notes.",
-    "And here you will see the statistics."
+    "And here you will see the statistics.",
+    "<h3>This icon shows that the note has an attachment. If you click it, you will see a preview in the note.</h3>"
   ],
   bg: [
     "Добре дошъли!<br>Аз съм <b>Lepcho</b> - вашият екскурзовод в света на лепящите бележки.",
     "Тук можеш да добавиш нови записи.",
-    "А тук ще видиш статистиката."
+    "А тук ще видиш статистиката.",
+    "<h3>Тази иконка показва, че към бележката има приложение. Ако я кликнеш, ще видиш умален вариант в бележката.</h3>",
+    ""
   ]
 };
 
@@ -27,7 +30,11 @@ const steps = [
   {
     image: "msm/1.png", height: 150, target: "#lang-bg-main", x: -198, y: 27, bx: 173, by: 47,
     textKey: 2
-  }
+  },
+  {
+    image: "msm/right-up.png", height: 150, target: "#search-box", x: -201, y: 292, bx: 80, by: -14,
+    textKey: 3
+  },
 ];
 
 let container;
@@ -301,7 +308,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if (appTitle) {
     appTitle.style.cursor = 'pointer';
     appTitle.addEventListener('click', () => {
-      showStep(0);
+      showStep(3, 3, true);
     });
   }
 });
