7e4aa80ced0543be3c597a7890918db40006b62b
cepreu5 <cepreu5@live.com>
2025-12-24 05:05:25 +0200

    assistant

--- uni/main.js
+++ uni/main.js
@@ -8,7 +8,7 @@
 // terser main.js  --compress arrows=true,booleans=true,collapse_vars=true,comparisons=true,dead_code=true,drop_console=true,hoist_funs=true,if_return=true,passes=3 --mangle --toplevel --ecma 2020 --module --format wrap_iife=true -c pure_funcs=["console.log"] --output mainn.js
 
 const version = '0.19'; // App version
-const debug = true; // Глобален флаг за дебъг режим
+const debug = false; // Глобален флаг за дебъг режим
 const msm = true;
 let guide = false;
 
