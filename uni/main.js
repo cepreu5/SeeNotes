c484ffa2583655c3c6da59763bf0a584208728d8
cepreu5 <cepreu5@live.com>
2026-04-21 01:41:16 +0300

    1.19

--- uni/main.js
+++ uni/main.js
@@ -7,8 +7,8 @@
 
 // terser main.js  --compress arrows=true,booleans=true,collapse_vars=true,comparisons=true,dead_code=true,drop_console=true,hoist_funs=true,if_return=true,passes=3 --mangle --toplevel --ecma 2020 --module --format wrap_iife=true -c pure_funcs=["console.log"] --output mainn.js
 
-const version = 'Beta 1.18'; // App version
-const debug = true; // Глобален флаг за дебъг режим
+const version = 'Beta 1.19'; // App version
+const debug = false; // Глобален флаг за дебъг режим
 window.isAppErrorState = false; // Флаг за грешки (изтекъл сертификат и др.)
 
 let guide = true;
